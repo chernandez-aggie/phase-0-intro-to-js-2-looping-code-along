deae8d911721bae94605f842fd5caa0973143bfc
candacehernandez <chernandez.aggie@gmail.com>
2022-02-08 16:46:02 -0600

    commit tree d31f145d86e7c94de7789135a6b09709695e1c90

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const gifts =["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(arrayName, eventName){
+    const newArray = [];
+    for (let i = 0; i < arrayName.length; i++){
+        newArray[i] = `Thank you, ${arrayName[i]}, for the wonderful surprise gift!`;
+    }
+    return newArray;
+}
+let integer;
+function countDown(integer){
+    while (integer >= 0){
+        console.log(integer);
+        integer--;
+    }
+}
